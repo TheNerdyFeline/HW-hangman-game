155ea8c0f876f21eebb52385a7fe35aab68f324e
Jackie Wellons <jackie@localhost.localdomain>
2017-02-17 16:55:09 -0800

    game almost complete. backing up progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,20 +6,21 @@ var word = "";
 var blanks = 0;
 var lettersInWord = [];
 var successes = [];
-var height = "15px";
+var riverHeight = 15;
 
 /* easy dictionary of words */
-var easyWords = ["bear", "cat", "dog", "tiger", "lion", "monkey", "panther", "rabbit", "snake", "spider"];
+var easyWords = ["ant", "anteater", "antelope", "bat", "bear", "beagle", "beaver", "bee", "beetle", "bird", "bulldog","bobcat", "bullfrog", "butterfly", "cat", "catfish", "camel", "cheetah", "chicken", "chipmunk", "cougar", "cow", "crab", "crane", "crocodile", "deer", "dingo", "dodo", "dog", "dolphin", "donkey", "duck", "eagle", "earwig", "eel","elephant", "emu", "falcon", "ferret", "fish", "flamingo", "flounder", "fly", "frog", "fox", "gorilla", "hornet", "leopard", "lion", "lizard", "monkey", "ostrich", "owl", "panther", "penguin", "rabbit", "seal","shark", "snake", "spider",  "squirrel", "tiger", "toad"];
 
-var mdWords = ["aardvark", "ostrich", "dolphin"];
+var mdWords = ["aardvark", "albatross", "alligator", "abyssinian", "angelfish", "armadillo", "axolotl", "barnacle", "bison", "bloodhound", "bombay", "buzzard", "capybara", "caterpillar",  "centipede", "chameleon", "chihuahua", "chimpanzee", "chinchilla", "cocker spaniel", "cockroach","cuttlefish", "dormouse", "dragonfly", "rhinoceros", ];
+
+var hardWords = ["adelie penguin", "affenpinscher", "arctic hare", "bearded dragon", "bengal tiger", "birds of paradise", "black bear", "black russian terrier", "black widow spider", "blue whale", "brown bear", "bumble bee", "butterfly fish", "clown fish", "darwins frog", "dusky dolphin", "dwarf crocodile", "electric eel", "emperor penguin", "elephent seal", "elephent shrew", "fennec fox", "fin whale", "fire-bellied toad", "flying squirrel",];
 
 function startGame() {
-
     if (level === 1) {
-
-	lettersGuessed = [];
 	successes = [];
-	
+	lettersGuessed = [];
+	timeLeft = 10;
+	riverHeight = 15;
 	
 	word = easyWords[Math.floor(Math.random() * easyWords.length)];
 	/* splits word into indivual leters and put into an array */
@@ -29,13 +30,14 @@ function startGame() {
 	for (var i = 0; i < lettersInWord.length; i++) {
 	    successes.push("_");
 	}
+	console.log(word);
+	roundComplete();
     } // closes if statement inside startGame
-
+    
     if (level === 2) {
 	lettersGuessed = [];
 	successes = [];
-	timeLeft = 10;
-	wins = 0;
+	riverHeight = 15;
 
 	word = mdWords[Math.floor(Math.random() * mdWords.length)];
 	lettersInWord = word.split("");
@@ -43,16 +45,17 @@ function startGame() {
 	    successes.push("_");
 	}
     }
-	/* puts user info into html for user */
-	document.getElementById("guessWord").innerHTML = successes.join(" ");
-	document.getElementById("timeToFlood").innerHTML = timeLeft;
-	document.getElementById("wins").innerHTML = wins;
-	document.getElementById("level").innerHTML = level;
-	document.getElementById("river").style.height = height;
-	/* testing random word function */
-	console.log(word);
 
-} // closes Start Game
+    /* testing random word function */
+    console.log(word);
+    /* puts user info into html for user */
+    document.getElementById("guessWord").innerHTML = successes.join(" ");
+    document.getElementById("timeToFlood").innerHTML = timeLeft;
+    document.getElementById("wins").innerHTML = wins;
+    document.getElementById("level").innerHTML = level;
+    document.getElementById("river").style.height = riverHeight + "px";
+    
+}; /* end function */
 
 /* this is seeing if letter typed is in word */
 function letterCheck(letter) {
@@ -60,57 +63,68 @@ function letterCheck(letter) {
        if (letter != lettersInWord[i] && !lettersGuessed.includes(letter) && !lettersInWord.includes(letter)) {
 	   lettersGuessed.push(letter);
 	   timeLeft--;
-	   console.log(timeLeft);
-	   document.getElementById("timeToFlood").innerHTML = timeLeft;
-	   document.getElementById("wrongLetters").innerHTML = lettersGuessed.join(" ");
+	   riverHeight += 15;
+	   console.log(riverHeight);
 	   
-       } else if (letter === lettersInWord[i]) {
+       } else if (lettersInWord[i] === letter) {
 	   successes[i] = letter;
 	   document.getElementById("guessWord").innerHTML = successes.join(" ");
-	   return true;
+	   
        } else {
-	   roundComplete();
+	   
        } // close if statement
    } // close for loop
+
+    
+    document.getElementById("wrongLetters").innerHTML = lettersGuessed.join(" ");
+    document.getElementById("timeToFlood").innerHTML = timeLeft;
+    document.getElementById("river").style.height = riverHeight + "px";
+    console.log("letters guessed: " + lettersGuessed);
+    console.log("right letters: " + successes);
     roundComplete();
 }; // closes letterCheck
-
-document.getElementById("wrongLetters").innerHTML = lettersGuessed.join(" ");
-console.log("letters guessed: " + lettersGuessed);
-console.log("right letters: " + successes);
-
 
 function roundComplete() {
     if (wins === 3){
 	level++;
+	wins = 0;
+	timeLeft = 10;
 	console.log("level up");
 	document.getElementById("level").innerHTML = level;
+	document.getElementById("wins").innderHTML = wins;
 	startGame();
 	
     } else if (lettersInWord.join("") === successes.join("")) {
 	wins++;
+	console.log(lettersInWord);
+	console.log(successes);
 	document.getElementById("wins").innerHTML = wins;
+	lettersGuessed = [];
 	startGame();
 	
     } else if (timeLeft === 0) {
+	lettersGuessed = [];
 	alert("You Lose!");
 	startGame();
     } else {
     }
-}
-
-startGame();
+    
+    document.getElementById("wrongLetters").innerHTML = lettersGuessed.join(" ");
+    document.getElementById("timeToFlood").innerHTML = timeLeft;
+    document.getElementById("river").style.height = riverHeight + "px";
+};
 
 document.onkeyup = function(event){
     if (event.keyCode >= 65 && event.keyCode <= 122) {
-    var letterPicked = String.fromCharCode(event.keyCode).toLowerCase();
-    console.log("letter picked, " + letterPicked);
-    letterCheck(letterPicked);
-	roundComplete();
+	var letterPicked = String.fromCharCode(event.keyCode).toLowerCase();
+	console.log("letter picked, " + letterPicked);
+	letterCheck(letterPicked);
+
     } else {
 	alert("Not a letter");
     }
     
 };
+
 
 
